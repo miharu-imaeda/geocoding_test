3284ebefafc5fba6b977f7bf5520912c9f944d2e
bp16009 <bp16009@shibaura-it.ac.jp>
2018-10-03 18:41:43 +0900

    スペルミス修正

--- tutorial/geocoding.py
+++ tutorial/geocoding.py
@@ -7,16 +7,16 @@
 import requests
 import xmltodict
 
-full_adress = "芝浦工業大学"
+full_address = "芝浦工業大学"
 url = 'http://www.geocoding.jp/api/'
-payload = {'q': full_adress}
+payload = {'q': full_address}
 result = requests.get(url, params=payload)
 
 
 resultdict = xmltodict.parse(result.text)
 resultdict
 
-print( full_adress )
+print( full_address )
 print("緯度：" + resultdict["result"]["coordinate"]["lat"])
 print("経度："+ resultdict["result"]["coordinate"]["lng"])
 
